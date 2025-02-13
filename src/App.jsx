4fa8e46f654d68b3a8b12b4bcccadb9dd779cf58
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-02-13 22:00:26 +0000

    Added some titles and paragraphs for each component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Cards } from "./components/cards/Cards.jsx";
 import { Header } from "./components/header/Header.jsx";
+import { ScoreBoard } from "./components/scoreboard/Scoreboard.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <ScoreBoard />
+      <Cards />
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,10 @@
 function Header() {
-  return <h1>Memory Card Game</h1>;
+  return (
+    <>
+      <h1>Memory Card Game</h1>
+      <p>Memory card game, don't click the same one twice!</p>
+    </>
+  );
 }
 
 export { Header };

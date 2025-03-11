da7e13b0f9be2e7f55e8ea05aced83d544ea1a3e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-03-11 17:42:40 +0000

    fixed the ScoreBoard import
    Closing bracket in the wrong place

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 import { Cards } from "./Cards";
-import { ScoreBoard from } "./Scoreboard";
+import { ScoreBoard } from "./Scoreboard";
 
 function Game() {
-  const [imageId, setImageId] = useState([])
+  const [imageId, setImageId] = useState([]);
 
   return (
     <div>
       <ScoreBoard currentScore={imageId.length} />
       <Cards imageId={imageId} setImageId={setImageId} />
     </div>
-  )
+  );
 }
 
 export { Game };

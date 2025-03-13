7a3f733a1a916f7239919b461c8b86fee5f7cfa9
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-03-13 17:42:15 +0000

    Position Header and ScoreBoard
    Added the <Header /> to <Game /> component
    Everything is rendered inside of the Game.jsx parent component which is then solely rendered in App.jsx
    Wraped the Header and ScoreBoard components inside of their own wrapper to make them flex wit space-between

--- src/components/scoreboard/Scoreboard.jsx
+++ src/components/scoreboard/Scoreboard.jsx
@@ -1,11 +1,10 @@
 function ScoreBoard({ currentScore, bestStreak }) {
   return (
-    <>
+    <div className="scoreboard-container">
       <h2>Score Board</h2>
-
       <h3>Best Streak: {bestStreak}</h3>
       <h3>Current Score: {currentScore}</h3>
-    </>
+    </div>
   );
 }
 
